1996c38dd837656d61f19e65b6b7b71ee1d10df8
nhannn87dn <nhannn87dn@gmail.com>
2025-04-08 18:40:50 +0700

    contect db

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
 import app from "./app";
+import mongoose from "mongoose";
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
-})
+const MONGO_CONNECTION_STRING = 'mongodb+srv://nhan:<db_password>@mongodbnodejs.51dpy.mongodb.net/?retryWrites=true&w=majority&appName=MongodbNodeJs'
+
+mongoose
+  .connect(MONGO_CONNECTION_STRING)
+
+  .then(() => {
+    console.log("Connected to MongoDB");
+    //should listen app here
+    app.listen(PORT, () => {
+      console.log(`Server is running on port http://localhost:${PORT}`);
+    })
+  })
+  .catch((err) => {
+    console.error("Failed to Connect to MongoDB", err);
+  });
+
